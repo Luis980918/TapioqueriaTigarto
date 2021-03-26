06e5e5305e921d16d8611f68e868469726213fee
Luis Carlos <lcarlos.rendon@udea.edu.co>
2021-03-26 14:05:11 -0500

    Calculando matriz de platos

--- TapioqueriaT.c
+++ TapioqueriaT.c
@@ -5,16 +5,28 @@
 #include <string.h>
 
 void dinamico(char *ingrediente);
+int primerRecorrido();
+int pp;
+void segundoRecorrido(int pp, int i);
 int contU=0;
 
+
 int main(void){
+	printf("\n %d \n", primerRecorrido());
+	printf("\n %d \n", contU);
+	segundoRecorrido(pp, contU);
+	remove("v.txt");
+	return 0;
+}
+
+
+/*En este metodo se lleva a cabo el primer recorrido del archivo o la primer lectura*/
+int primerRecorrido(){
 	FILE* flujo=fopen("pedidos.txt", "rb");
 	if(flujo==NULL){
 		perror("Error en la apertura del archivo");
 		return 1;
 	}
-	char *vectordinamico[1000];
-	int pp;
 	int acum=0;
 	int contador=1;
 	char cadena[100];
@@ -35,13 +47,70 @@ int main(void){
 				dinamico(cadena);
 		}
 	}
-	printf("\n %d",acum);
-	printf("\n %d \n", contU);
 	fclose(flujo);
-	remove("v.txt");
-	return 0;
+	return acum;
 }
 
+/*Algoritmo para hacer la matriz*/
+void segundoRecorrido(int pp, int i){
+
+	
+	FILE* flujo=fopen("pedidos.txt", "rb");
+	char cadena2[100];
+	int contador=1;
+	int control=1;
+	char cadena[100];
+	int acum=0;
+	int matriz[pp][i];
+
+	for(int j=0;j<i;j++){
+		for(int k=0;k<pp;k++){
+			matriz[k][j]=0;
+		}
+	}
+	while(feof(flujo)==0){
+		fscanf(flujo, "%s\n", cadena);
+		if(isdigit(*cadena)){
+			if(contador>4){
+				//printf("\n%d\n", contador-4);
+				acum=atoi(cadena);
+			}
+			contador++;
+		}else{
+			/*if(strcmp(cadena, "ajo")==0){
+				printf("\n%d\n", contador-5);
+				printf("\n%s\n",cadena);
+			}*/
+
+			FILE * flujo3=fopen("v.txt", "rb");
+			int b=0;
+			while(feof(flujo3)==0){
+				fscanf(flujo3, "%s\n", cadena2);
+				if(strcmp(cadena2, cadena)==0){
+					matriz[contador-6][b]=1;
+					//printf("\n%d\n",b);
+				}
+				b++;
+			}
+			fclose(flujo3);
+
+		}
+	}
+	for(int j=0;j<i;j++){
+		for(int k=0;k<pp;k++){
+			printf(" %d", matriz[k][j]);
+		}
+		printf("\n");
+	}
+
+	fclose(flujo);
+
+}	
+
+
+
+/*En este metodo se utiliza un archivo de texto plano para almacenar
+la información dinamicamente y saber la totalidad de platos distintos*/
 void dinamico(char *dato){
 
 	int bandera=1;
